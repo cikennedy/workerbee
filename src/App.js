a90a51e1e0f1bd2d1b10d0919d0293f0659525a4
johnhumms <johnehummeljr@gmail.com>
2021-06-28 21:40:50 -0500

    added sign in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
+import SignInSide from './pages/signin.js'
 import './App.css';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <Navbar /> */}
+          <Route exact path="/" component={SignInSide} />
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
