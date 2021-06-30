9d378b54caf2762bd794189b9bef11a1092f80de
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-06-30 16:09:16 -0500

    added route paths in app.js src file for home, account, post job future pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from "react";
 import SignInSide from './pages/signin.js'
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -8,10 +9,11 @@ function App() {
       <div>
         {/* <Navbar /> */}
           <Route exact path="/" component={SignInSide} />
+          {/* <Route exact path="/home" component={Home} />
+          <Route exact path="/account" component={Account} />
+          <Route exact path="/post-job" component={PostJob} /> */}
         {/* <Footer /> */}
       </div>
     </Router>
   );
 }
-
-export default App;
