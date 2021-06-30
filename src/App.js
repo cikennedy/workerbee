9993dedf212d01c5d6b32bf6e9fb67f8cbf3744e
johnhumms <johnehummeljr@gmail.com>
2021-06-30 17:07:54 -0500

    added /home link to album template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import React from "react";
 import SignInSide from './pages/signin.js'
+import Album from './pages/home.js'
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
@@ -9,8 +10,8 @@ function App() {
       <div>
         {/* <Navbar /> */}
           <Route exact path="/" component={SignInSide} />
-          {/* <Route exact path="/home" component={Home} />
-          <Route exact path="/account" component={Account} />
+          <Route exact path="/home" component={Album} />
+          {/* <Route exact path="/account" component={Account} />
           <Route exact path="/post-job" component={PostJob} /> */}
         {/* <Footer /> */}
       </div>
