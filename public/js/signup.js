77c273b92d3196f4a80b3027b86216afe9aacd20
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-06-30 19:27:11 -0500

    updated user api routes with login, logout, signup, delete funcitonality. still requires testing

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,33 +1,33 @@
-// // Signup form that is similar to login, but includes email information
-// async function signupFormHandler(event) {
-//       // Stop the browser from submitting the form so we can do so with JavaScript
-//       event.preventDefault();
+// Signup form that is similar to login, but includes email information
+async function signupFormHandler(event) {
+      // Stop the browser from submitting the form so we can do so with JavaScript
+      event.preventDefault();
     
-//       // Gather the data from the form elements on the page
-//       const owner_name = document.querySelector('#name-signup').value.trim();
-//       const email = document.querySelector('#email-signup').value.trim();
-//       const password = document.querySelector('#password-signup').value.trim();
+      // Gather the data from the form elements on the page
+      const username = document.querySelector('#username').value.trim();
+      const email = document.querySelector('#email').value.trim();
+      const password = document.querySelector('#password').value.trim();
     
-//       if (owner_name && email && password) {
-//         // Send the username, email, and password to the server
-//         const response = await fetch('/api/users', {
-//           method: 'POST',
-//           body: JSON.stringify({
-//             owner_name,
-//             email,
-//             password
-//           }),
-//           headers: { 'Content-Type': 'application/json' },
-//         });
+      if (username && email && password) {
+        // Send the username, email, and password to the server
+        const response = await fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify({
+            username,
+            email,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' },
+        });
     
-//         // Sends user to the dashboard if the login succeeds
-//         if (response.ok) {
-//           console.log('Account Generated.')
-//           document.location.replace('/dashboard');
-//         } else {
-//           alert(response.statusText);
-//         }
-//       }
-//     };
+        // Sends user to the home page if the login succeeds
+        if (response.ok) {
+          console.log('Account Generated.')
+          document.location.replace('/home');
+        } else {
+          alert(response.statusText);
+        }
+      }
+    };
   
-//     document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+    document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
