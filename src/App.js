b05e63561bc8917af8790b21110170046dbe412d
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-06-30 20:42:25 -0500

    added signup routing, link to signup page functional

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React from "react";
 import SignInSide from './pages/signin.js'
 import Album from './pages/home.js'
+import SignUp from './pages/signup.js'
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
@@ -11,6 +12,7 @@ function App() {
         {/* <Navbar /> */}
           <Route exact path="/" component={SignInSide} />
           <Route exact path="/home" component={Album} />
+          <Route exact path="/signup" component={SignUp} />
           {/* <Route exact path="/account" component={Account} />
           <Route exact path="/post-job" component={PostJob} /> */}
         {/* <Footer /> */}
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -109,13 +109,13 @@ export default function SignInSide() {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
+              {/* <Grid item xs>
+                <Link href="/forgotpassword" variant="body2">
                   Forgot password?
                 </Link>
-              </Grid>
+              </Grid> */}
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
