94db94c9cdeab11517a2aa5f360fef0f77804df1
Kevin Jun <kwjun90@gmail.com>
2021-07-14 19:07:46 -0500

    added to API.js in utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,20 @@
 import axios from "axios";
 
-
+export default {
+    // Gets all jobs
+    getJobs: function() {
+      return axios.get("/api/jobs");
+    },
+    // Gets the Job with the given id
+    getJob: function(id) {
+      return axios.get("/api/jobs/" + id);
+    },
+    // Deletes the Job with the given id
+    deleteJob: function(id) {
+      return axios.delete("/api/jobs/" + id);
+    },
+    // Saves a Job to the database
+    saveJob: function(jobData) {
+      return axios.post("/api/jobs", jobData);
+    }
+  };
