9a05ea202cb96ce6657700ef399c5d90796b62b3
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-17 14:02:52 -0500

    sign in, sign out functionality added. sign in functionality tested and working, need to add sign out button in order to test

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -64,7 +64,7 @@ function signUp() {
     const password = formObject.password;
 
     if (username && email && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/users/signup', {
         method: 'POST',
         body: JSON.stringify({
           username,
