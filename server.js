d81a196151b206baa7e1443fa406622f863b2e1f
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-19 18:53:02 -0500

    changed server.js file structure

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Add routes, both API and view
+app.use(routes);
+
 // Define middleware here
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -17,8 +20,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
-app.use(routes);
+
 
 // Connect to the Mongo DB
 mongoose.connect(
