47cf76994e225bc5f93146cf444d928bc25022c5
johnhumms <johnehummeljr@gmail.com>
2021-07-19 19:06:25 -0500

    removed unnecessary text in footer

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -205,12 +205,12 @@ export default function HomePage() {
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
+        {/* <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
           Something here to give the footer a purpose!
-        </Typography>
+        </Typography> */}
         <Copyright />
       </footer>
       {/* End footer */}
