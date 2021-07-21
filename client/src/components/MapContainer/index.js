84c420ab5e6f3faeb7c6becbebe8caceec171ef0
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-21 17:01:09 -0500

    hiding api key

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -4,7 +4,7 @@ import Geocode from "react-geocode";
 import axios from "axios";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
-const API_KEY = process.env.GoogleAPIKey;
+const API_KEY = process.env.API_KEY;
 
 class Map extends Component {
   static defaultProps = {
@@ -39,34 +39,33 @@ class Map extends Component {
       );
   };
 
-  // componentDidMount = () => {
-  //   // Google Geocoder
-  //   // Geocode.setApiKey(API_KEY);
-  //   Geocode.setApiKey("AIzaSyCPhWnebKd5wNSgnlUres5WdKGQdwc1jS0");
-  //   Geocode.setLanguage("en");
+  componentDidMount = () => {
+    // Google Geocoder
+    // Geocode.setApiKey(API_KEY);
+    Geocode.setApiKey("AIzaSyCPhWnebKd5wNSgnlUres5WdKGQdwc1jS0");
+    Geocode.setLanguage("en");
 
-  //   // set this as US
-  //   Geocode.setRegion("us");
+    // set this as US
+    Geocode.setRegion("us");
 
-  //   // Get latitude & longitude from address
-  //   Geocode.fromAddress(this.state.address, i) .then(
-  //     (response) => {
-  //       const { lat, lng } = response.results[0].geometry.location;
-  //       console.log(lat, lng);
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-  // };
+    // Get latitude & longitude from address
+    Geocode.fromAddress(this.state.address, i) .then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  };
 
   render() {
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: "100vh", width: "100%" }}>
         <GoogleMapReact
-          // bootstrapURLKeys={{ key: API_KEY }}
-          bootstrapURLKeys={{ key: "AIzaSyCPhWnebKd5wNSgnlUres5WdKGQdwc1jS0" }}
+          bootstrapURLKeys={{ key: API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
